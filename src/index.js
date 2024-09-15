4dcacf4078cca50b3b2003198da3c3986d44edbd
Wellington Feitoza <spaaws@gmail.com>
2024-09-15 13:29:33 -0300

    adjusts

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// import './index.css'; // Comente ou remova esta linha se não for necessário
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
